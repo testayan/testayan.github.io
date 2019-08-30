fcb4979d3756ffef1ec23f241ead49f2b6f16a31
Ayan Bag <ayanbag9474@gmail.com>
2019-08-30 18:56:47 +0530

    4

--- login/js/login.js
+++ login/js/login.js
@@ -1,8 +1,32 @@
-function login(){
-
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-    window.alert("Logedin")
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+  
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(user != null){
+  
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+  
+      }
+  
+    } else {
+  
+  
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+  
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("usnm").value;
+    var userPass = document.getElementById("pswd").value;
+  
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   
       var errorCode = error.code;
@@ -14,3 +38,8 @@ function login(){
     });
   
   }
+  
+  function logout(){
+    firebase.auth().signOut();
+  }
+  
