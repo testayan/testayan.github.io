962de1974ba3df0296d928cbf4c99bb3520007ba
Ayan Bag <ayanbag9474@gmail.com>
2019-08-30 19:55:01 +0530

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -9,7 +9,10 @@ function login(){
     var userEmail = document.getElementById("usnm").value;
     var userPass = document.getElementById("pswd").value;
   
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then(user=>{
+        window.location = '../index.html';
+     } ).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
